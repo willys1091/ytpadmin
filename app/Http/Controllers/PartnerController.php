583ea5e13bc3c5db85c47f1a518b9b53b9b69ff6
willys <w1llys_910710@yahoo.co.id>
2021-03-25 16:54:26 +0700

    modul partner process

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
+use App\Models\partner;
 
-class PartnerController extends Controller
-{
-    //
+class PartnerController extends Controller{
+    public function index(){
+        $data['title'] = "Partner | Tangan Pengharapan";
+        $data['subtitle'] = "List of Partner";
+        $data['contentHeader'] = "mdl";
+        $data['btn'] = array('title' => 'Add Partner', 'url' => 'partner/create', 'icon' => 'fas fa-plus');
+        $data['partner'] = event::all();
+        return view('partner.index',$data);
+    }
+
+    public function create(){
+        $data['title'] = "Partner | Tangan Pengharapan";
+        $data['action'] = "add";
+        return view('partner.action',$data);
+    }
+
+    public function store(Request $request){
+        //
+    }
+
+    public function show($id){
+        //
+    }
+
+    public function edit($id){
+        $data['title'] = "Partner | Tangan Pengharapan";
+        $data['action'] = "edit";
+        $data['data'] = partner::findorfail($id);
+        return view('partner.action',$data);
+    }
+
+    public function update(Request $request, $id){
+        //
+    }
+
+    public function destroy($id){
+        //
+    }
 }
---
+++ app/Http/Livewire/PartnerImg.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PartnerImg extends Component
+{
+    public function render()
+    {
+        return view('livewire.partner-img');
+    }
+}
