763d326852aac05b0b275a0d8b7be57f58ef13c6
willys <w1llys_910710@yahoo.co.id>
2021-03-28 19:52:19 +0700

    people modif

--- resources/views/people/action.blade.php
+++ resources/views/people/action.blade.php
@@ -12,67 +12,43 @@
         <form action="{{url('people/'.$data->id)}}" method="post"> @method('patch')
         <input type="hidden" name="id" value="{{$data->id}}"/>
     @endif @csrf
-
-    <input type="hidden" name="modul" value="{{$modul}}"/>
     <div class="block-content font-size-sm">
         <div class="row">
-            @livewire('people-email', ['action' => $action , 'dataemail' => $data->email ?? 0])
-            
-            @if($modul=='admin')
-                @livewire('people-type', ['action' => $action , 'datatype' => $data->type ?? 0, "id_field" => "people_type"])
-            @elseif($modul =='user')
-                @livewire('people-gender', ['action' => $action , 'datagender' => $data->gender ?? 0])
-            @endif
-        </div>
-
-        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label for="Name">Email <span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" name="email" placeholder="Email" value="{{$action=='edit'?$data->email:''}}" required>
+                </div>
+            </div>
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="Name">Name <span class="text-danger">*</span></label>
                     <input type="text" class="form-control" name="name" placeholder="Name" value="{{$action=='edit'?$data->name:''}}" required>
                 </div>
             </div>
-            @if($modul=='admin')
-                @livewire('people-role', ['action' => $action , 'datarole' => $data->roleid ?? 0, 'datatype' => $data->type ?? 0])
-            @else
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <label for="Name">Language </label>
-                        <input type="text" class="form-control" name="language" placeholder="Language" value="{{$action=='edit'?$data->language:''}}" >
-                    </div>
-                </div>
-            @endif
         </div>
-        @if($modul=='admin')
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <label for="Name">Title </label>
-                        <input type="text" class="form-control" name="title" placeholder="Title" value="{{$action=='edit'?$data->title:''}}" >
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <label for="Password">Password </label>
-                        <input type="password" class="form-control" name="password" placeholder="Password">
-                    </div>
-                </div>
-            </div>   
-        @endif
 
-        @livewire('people-button',['action' => $action])
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label for="Password">Password </label>
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+            </div>
+        </div> 
+
+        <div class="block-content block-content-full text-right border-top" >
+            <button type="button" class="btn btn-alt-primary mr-1" data-dismiss="modal">Close</button>
+            <button type="submit"  class="btn btn-primary">Save</button> 
+        </div>
     </div> 
     </form>
 </div>
-<script src="{{asset('public/js/plugins/select2/js/select2.full.min.js')}}"></script>
-<script src="{{asset('public/js/plugins/flatpickr/flatpickr.min.js')}}"></script>
-@livewireStyles
-@livewireScripts
-@stack('scripts')
+<script src="{{asset('js/plugins/select2/js/select2.full.min.js')}}"></script>
+<script src="{{asset('js/plugins/flatpickr/flatpickr.min.js')}}"></script>
 <script>
-    Livewire.restart();
    
     jQuery(function () {
-        Siap.helpers(['flatpickr', 'select2']);
+        Siap.helpers(['flatpickr']);
     });
 </script>
--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -7,11 +7,8 @@
                 <thead>
                     <tr>
                         <th class="text-center">ID</th>
-                        {!! $modul=='user'?'<th>Avatar</th>':'' !!}
                         <th>Email</th>
                         <th>Name</th>
-                        {!! $modul=='admin'?'<th>Title</th><th>type</th>':'' !!}
-                        {!! $modul=='user'?'<th>Source</th><th>Refferal</th>':'' !!}
                         <th></th>
                     </tr>
                 </thead>
@@ -20,18 +17,12 @@
                     @foreach($people as $p)
                     <tr>
                         <td class="text-center font-size-sm">{{$x}}</td>
-                        @if($modul=='user')<td class="d-sm-table-cell font-size-sm"><img src='{{$p->avatar}}' class="rounded-circle" width="100px" height="100px"></td> @endif
                         <td class="d-sm-table-cell font-size-sm">{{$p->email}}</td>
                         <td class="d-sm-table-cell font-size-sm font-w600"><a href="{{url('people/'.$p->id.'/detail')}}">{{ucwords($p->name)}}</a></td>
-                        @if($modul=='admin')
-                            <td class="d-sm-table-cell font-size-sm">{{$p->title}}</td>
-                            <td class="d-sm-table-cell font-size-sm"><span class="badge badge-{{$p->type=='admin'?'danger':'success'}}">{{ucwords($p->type)}}</span></td>
-                        @endif
-                        @if($modul=='user')<td class="d-sm-table-cell font-size-sm"><span class="badge badge-{{$p->source=='google'?'danger':'primary'}}">{{ucwords($p->source)}}</span> </td> <td class="d-sm-table-cell font-size-sm">{!!$p->refferal<>''?$p->reff->name:'<em class="text-muted font-size-sm">None</em>'!!}</td> @endif
                         <td class="text-center">
                             <div class="btn-group">
-                                @if($modul=='admin') @livewire('active', ['status' => $p->active,'modul'=> $modul,'key'=> $p->id]) @endif
-                                <a onClick='showM("{{url('people/'.$modul.'/'.$p->id.'/edit')}}");return false' type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Edit"><i class="fas fa-fw fa-pencil-alt"></i></a>
+                                 @livewire('active', ['status' => $p->active,'modul'=> 'admin','key'=> $p->id])
+                                <a onClick='showM("{{url('people/'.$p->id.'/edit')}}");return false' type="button" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Edit"><i class="fas fa-fw fa-pencil-alt"></i></a>
                             </div>
                         </td> 
                     </tr>
@@ -46,32 +37,31 @@
 
 @section ('headerScript')
     @livewireStyles
-    <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/datatables/dataTables.bootstrap4.css')}}">
-    <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/datatables/responsive.bootstrap4.min.css')}}">
-    <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/datatables/buttons-bs4/buttons.bootstrap4.min.css')}}">
-    <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/select2/css/select2.min.css')}}">
-    <link rel="stylesheet" id="css-main" href="{{asset('public/js/plugins/flatpickr/flatpickr.min.css')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset('js/plugins/datatables/dataTables.bootstrap4.css')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset('js/plugins/datatables/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset('js/plugins/datatables/buttons-bs4/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset('js/plugins/select2/css/select2.min.css')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset('js/plugins/flatpickr/flatpickr.min.css')}}">
 @endsection
 
 @section ('footerScript')
     @livewireScripts
     <script>Livewire.restart();</script>
-    <script src="{{asset('public/js/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('public/js/plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('public/js/plugins/datatables/buttons/dataTables.buttons.min.js')}}"></script>
-    <script src="{{asset('public/js/plugins/datatables/buttons/buttons.print.min.js')}}"></script>
-    <script src="{{asset('public/js/plugins/datatables/buttons/buttons.html5.min.js')}}"></script>
-    <script src="{{asset('public/js/plugins/datatables/buttons/buttons.flash.min.js')}}"></script>
-    <script src="{{asset('public/js/plugins/datatables/buttons/buttons.colVis.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/buttons/dataTables.buttons.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/buttons/buttons.print.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/buttons/buttons.html5.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/buttons/buttons.flash.min.js')}}"></script>
+    <script src="{{asset('js/plugins/datatables/buttons/buttons.colVis.min.js')}}"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.6/js/responsive.bootstrap4.min.js"></script>
-    <script src="{{asset('public/js/siapfulin.js')}}"></script>
-    <script src="{{asset('public/js/plugins/select2/js/select2.full.min.js')}}"></script>
-@stack('scripts')
+    <script src="{{asset('js/siapfulin.js')}}"></script>
+    <script src="{{asset('js/plugins/select2/js/select2.full.min.js')}}"></script>
 <script>
     
     jQuery(function () {
-        Siap.helpers(['flatpickr', 'select2']);
+        Siap.helpers(['flatpickr']);
     });
 </script>
 @endsection
