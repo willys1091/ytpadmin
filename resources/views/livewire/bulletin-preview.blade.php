634f222006b884bedaf3d184ef117849aa177d5e
willys1091 <w1llys_910710@yahoo.co.id>
2021-05-09 19:31:21 +0700

    bulletin fix

--- resources/views/livewire/bulletin-preview.blade.php
+++ resources/views/livewire/bulletin-preview.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
-    {{-- <div class="form-group col-sm-6">
+    <div class="form-group col-sm-6">
         <div class="form-group">
             <label for="Name">URL</label>
             <input wire:model='url' type="text" class="form-control" name="url" placeholder="URL">
         </div>
-    </div> --}}
+    </div>
     <div class="form-group col-sm-6" x-data="{ filenames: @entangle('filenames') }">
         <div class="form-group" >
             <span @getimg.window="filenames = $event.detail.filename" x-model="filenames"></span>
